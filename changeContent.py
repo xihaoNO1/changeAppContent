a199788c8374c48c9a7f8eda50d948ba3335d388
xihaoNO1 <2234574497@qq.com>
2018-08-04 11:21:07 +0800

    更新脚本

--- changeContent.py
+++ changeContent.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os
@@ -11,14 +11,14 @@ def addContent():
 
     # 获取目标路径
     path = sys.path[0]
-    print path
-    print '内容修改中....'
+    print(path)
+    print('内容修改中....')
     changeFile(path)
     showComplete()
 
 def showComplete():
     '''显示完成'''
-    print '***************   修改项目完成,可以打包了  ******************'
+    print ('***************   修改项目完成,可以打包了  ******************')
 
 def changeFile(filepath):
     '''修改文件'''
@@ -42,6 +42,7 @@ def changeFile(filepath):
                                index1 = max(index1,i)
 
                        content1.insert(index1, "\n" * num)
+                       os.remove(pathStr)
                        with open(pathStr, 'w') as f2:
                            f2.writelines(content1)
 
@@ -55,23 +56,34 @@ def changeFile(filepath):
                            if str == '@end\n' or str == '@end':
                                index2 = max(index2,i)
                        content2.insert(index2, "\n" * num)
+                       os.remove(pathStr)
                        with open(pathStr, 'w') as f4:
                            f4.writelines(content2)
 
             elif typeStr == 'png':
-                img = Image.open(pathStr).convert('RGBA')
+            	
+            	# #首先检测图片是否正常
+            	# if IsValidImage(pathStr) == False:
+            	# 	continue
+                try:
+                    img = Image.open(pathStr).convert('RGBA')
+                except IOError as e:
+                    continue
+
                 size = img.size
 
                 '''如果是1024上架图标,重新处理,去掉透明通道'''
                 if size == (1024,1024):
                     img = Image.open(pathStr).convert('RGB')
                     img.save(pathStr, 'PNG')
+                    os.remove(pathStr)
                 else:
                     pix = img.load()
                     for x in range(0, size[0]):
                         for y in range(0, size[1]):
                             r = random.randint(0, 1)
                             pix[x, y] = (pix[x, y][0] - r, pix[x, y][1], pix[x, y][2], pix[x, y][3])
+                    os.remove(pathStr)
                     img.save(pathStr, 'PNG')
 
 #                    img = Image.open(pathStr).convert('RGB')
@@ -84,18 +96,25 @@ def changeFile(filepath):
 #                    img.save(pathStr, 'PNG')
 
 
-
-
+'''检测图片是否完整'''
+def IsValidImage(pathfile):
+    bValid = True
+    try:
+        Image.open(pathfile).verify()
+    except:
+        print(pathfile)
+        bValid = False
+    return bValid
 
 def deleteContent():
     '''还原添加和修改的内容'''
     # 强制还原
     os.system('git reset --hard')
-    print '***************   还原项目完成  ******************'
+    print ('***************   还原项目完成  ******************')
 
 
 if __name__ == '__main__':
-    type = raw_input('请输入需要进行的操作'
+    type = input('请输入需要进行的操作'
                      ' (a 修改工程;'
                      'd 还原工程) : ')
 
--- gitPull.py
+++ gitPull.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
 path = input('请输入仓库地址:')
 os.chdir(path)
 list = os.popen('git branch','r').read()
+print(list)
 list = list.splitlines()
 for branch in list:
     os.system('git checkout %s' % branch)
